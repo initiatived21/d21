f1a93376f153f583ad301456e52212a4e919a72c
Marc Altmann <mail@marcaltmann.com>
2016-05-14 20:32:39 +0200

    Write separate pledge reducer with test

--- client/app/lib/reducers/combined.js
+++ client/app/lib/reducers/combined.js
@@ -1,35 +1,19 @@
-// { combineReducers } = require('redux')
-//
-// pledgeFormSubmit = require('./pledgeFormSubmit')
-// pledgeAddition = require('./pledgeAddition')
-//
-// module.exports = combineReducers({
-//   pledgeFormSubmit
-//   pledgeAddition
-// })
-
-import merge from 'lodash/merge';
+import _ from 'lodash';
 
 export default function(state = { pledges: {} }, action) {
   switch (action.type) {
     case 'ADD_ENTITIES':
-      return merge({}, state, action.entities);
+      return _.merge({}, state, action.entities);
 
     case 'SET_ENTITY':
-      let newState = merge({}, state);
+      let newState = Object.assign({}, state);
       newState[`${action.entityType}s`][action.entityId] = action.entity;
       return newState;
 
     case 'UPDATE_PLEDGE_ATTRIBUTE':
-      newState = merge({}, state);
+      newState = Object.assign({}, state);
       newState.pledges[action.id][action.attribute] = action.value;
       return newState;
-
-    // when 'SUBMIT_PLEDGE_FORM'
-    //   console.log('handling submitted pledge form', state)
-    //   retst = Object.assign {}, state, foo: action.formData.content
-    //   console.log retst
-    //   return retst
 
     default:
       return state;
