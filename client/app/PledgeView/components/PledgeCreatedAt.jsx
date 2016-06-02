7438a5eaeabbfe4fa663e2ea9c404ab8dcde2da4
Marc Altmann <mail@marcaltmann.com>
2016-06-02 18:35:53 +0200

    Format pledge created at date and time properly

--- client/app/PledgeView/components/PledgeCreatedAt.jsx
+++ client/app/PledgeView/components/PledgeCreatedAt.jsx
@@ -1,18 +1,31 @@
 import React, { PropTypes } from 'react';
-import I18n                 from 'i18n-js';
-import ChildComponent       from '../../lib/Base/components/ChildComponent.js';
+import I18n from 'i18n-js';
+import pad from 'pad-number';
+import ChildComponent from '../../lib/Base/components/ChildComponent.js';
 
 export default class PledgeCreatedAt extends ChildComponent {
   static propTypes = {
     children: PropTypes.string.isRequired
   };
 
+  getDateString() {
+    const date = new Date(this.props.children);
+
+    return `${pad(date.getDate(), 2)}.${pad((date.getMonth() + 1), 2)}.${date.getFullYear()}`;
+  }
+
+  getTimeString() {
+    const date = new Date(this.props.children);
+
+    return `${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)} Uhr`;
+  }
+
   render() {
     const { children } = this.props;
 
     return (
       <p className="c-pledge__created-at">
-        Veröffentlicht am {children}
+        Veröffentlicht am {this.getDateString()} um {this.getTimeString()}
       </p>
     );
   }
--- client/app/PledgeView/components/PledgeCreatedAt_test.js
+++ client/app/PledgeView/components/PledgeCreatedAt_test.js
@@ -5,9 +5,10 @@ import PledgeCreatedAt from './PledgeCreatedAt';
 describe('<PledgeCreatedAt />', function () {
   it('should render', function () {
     const wrapper = shallow(
-      <PledgeCreatedAt>2016-09-01T12:45:22.964Z</PledgeCreatedAt>
+      <PledgeCreatedAt>2016-09-01T12:05:22.964Z</PledgeCreatedAt>
     );
 
     wrapper.hasClass('c-pledge__created-at').should.be.true;
+    wrapper.text().should.equal('Veröffentlicht am 01.09.2016 um 14:05 Uhr');
   });
 });
