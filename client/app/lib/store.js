86ab01f55982cf901a91941e5d03bde3651d2d15
Marc Altmann <mail@marcaltmann.com>
2016-06-10 13:17:41 +0200

    Fix test exception and remove node_modules in root dir

--- client/app/lib/store.js
+++ client/app/lib/store.js
@@ -1,9 +1,8 @@
 // There shall only ever be one store according to the Redux design
 import reducer, { initialState } from './reducers/combined';
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk'
-import loggerMiddleware from 'lib/middlewares/loggerMiddleware'
-
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from './middlewares/loggerMiddleware';
 
 export default createStore(
   reducer,
