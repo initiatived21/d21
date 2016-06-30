daa7e206ad29ea6443d9f3fcc33f6118c07ebaa3
Marc Altmann <mail@marcaltmann.com>
2016-06-30 19:02:09 +0200

    Refactor SocialMediaButtons component

--- client/app/SocialMediaButtons/components/SocialMediaButtons.jsx
+++ client/app/SocialMediaButtons/components/SocialMediaButtons.jsx
@@ -9,23 +9,27 @@ import XingButton from './XingButton.jsx';
 import LinkedinButton from './LinkedinButton.jsx';
 
 export default class SocialMediaButtons extends ChildComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   handleClick(e) {
     e.preventDefault();
-
     const anchor = e.currentTarget;
-    const url = anchor.href;
+    this.openWindow(anchor.href);
+  }
 
+  openWindow(url) {
     const windowName = '_blank';
     const windowSizeX = '600';
     const windowSizeY = '460';
     const windowSize = `width=${windowSizeX},height=${windowSizeY}`;
-
     global.window.open(url, windowName, windowSize);
   }
 
   render() {
     const { url } = this.props;
-
     const commonProps = {
       url,
       handleClick: this.handleClick
