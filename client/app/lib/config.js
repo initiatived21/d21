518a3bf445dc843c2db8e29eb5583120837b1eb7
Marc Altmann <mail@marcaltmann.com>
2016-07-21 12:37:18 +0200

    For #67 set Facebook and Twitter account urls

--- client/app/lib/config.js
+++ client/app/lib/config.js
@@ -1,7 +1,9 @@
 /* Configuration of client app */
 
 /* Development */
-export const DUMMY_IMAGE_PATH = '/assets/images';
+export const DUMMY_IMAGE_PATH = '/assets/images'
 
 /* Production */
-export const DOMAIN_PROD = 'https://www.buntundverbindlich.de';
+export const DOMAIN_PROD = 'https://www.buntundverbindlich.de'
+export const FACEBOOK_ACCOUNT_URL = 'https://www.facebook.com/Buntundverbindlichde-895164520594209/'
+export const TWITTER_ACCOUNT_URL = 'https://twitter.com/buntverbindlich'
