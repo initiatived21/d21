6292ae2a0fa338ea2da5731142d3f459f75f21a0
Marc Altmann <mail@marcaltmann.com>
2016-07-21 18:30:24 +0200

    Make standalone button stylable

--- client/app/lib/Form/components/StandaloneButton.jsx
+++ client/app/lib/Form/components/StandaloneButton.jsx
@@ -6,16 +6,22 @@ export default class BaseForm extends ChildComponent {
     action: PropTypes.string.isRequired,
     method: PropTypes.string.isRequired,
     authToken: PropTypes.string.isRequired,
+    className: PropTypes.string
   }
 
   render() {
-    const { action, method, authToken, children } = this.props
+    const { action, method, authToken, children, className } = this.props
+
+    let combinedClassName
+    if (className) {
+      combinedClassName = className
+    }
 
     return(
-      <form action={action} method='POST'>
+      <form style={{ display: 'inline-block' }} action={action} method='POST'>
         <input type='hidden' name='authenticity_token' value={authToken} />
         <input type='hidden' name='_method' value={method} />
-        <button type='submit'>{children}</button>
+        <button className={className} type='submit'>{children}</button>
       </form>
     )
   }
