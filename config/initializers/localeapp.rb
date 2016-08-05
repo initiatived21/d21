3c723ce3ce17f94f813dd4c44d9b034f6d34dd7d
Konstantin K <konstantin.koss@gmail.com>
2016-08-05 15:47:30 +0200

    disable localeapp polling on production

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,8 +1,15 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.api_key                    = ENV['LOCALEAPP_KEY']
   config.translation_data_directory = 'config/locales'
   config.synchronization_data_file  = '.localeapp/log.yml'
   config.daemon_pid_file            = '.localeapp/localeapp.pid'
+
+  if ENV['LOCALEAPP_KEY']
+    config.api_key = ENV['LOCALEAPP_KEY']
+    config.polling_environments = [:development]
+  else
+    config.api_key = 'none'
+    config.polling_environments = []
+  end
 end
