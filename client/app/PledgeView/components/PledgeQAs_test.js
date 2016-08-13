7a220c33e306035480f83fb8aee67604043bc8c9
Marc Altmann <mail@marcaltmann.com>
2016-08-13 16:05:46 +0200

    Show message if there are no questions yet for #83

--- client/app/PledgeView/components/PledgeQAs_test.js
+++ client/app/PledgeView/components/PledgeQAs_test.js
@@ -4,14 +4,36 @@ import { Provider } from 'react-redux'
 import PledgeQAs from './PledgeQAs'
 
 describe('<PledgeQAs />', function () {
+  const props = {
+    comments: [
+      {
+        id: 1,
+        content: 'question1',
+        response: 'answer1'
+      },
+      {
+        id: 2,
+        content: 'question2'
+      }
+    ],
+    forms: {
+      questionForm: {},
+      answerForm: {}
+    },
+    userCanAskQuestions: true,
+    userCanAnswer: false,
+    isSubmitting: false
+  }
+
   it('should render', function () {
-    const forms = { questionForm: {}, answerForm: {} }
-    const wrapper = shallow(
-      <PledgeQAs comments={[]} forms={forms} userCanAskQuestions={true}
-        userCanAnswer={false} isSubmitting={false}
-      />
-    )
+    const wrapper = shallow(<PledgeQAs {...props} />)
 
     wrapper.find('div').length.should.equal(1)
+  })
+
+  it('should render a text message if there are no comments yet', function () {
+    const wrapper = shallow(<PledgeQAs {...props} comments={[]} />)
+
+    wrapper.find('p').length.should.equal(1)
   })
 })
