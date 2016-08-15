6fb3cf06a769f93eeb38800a89cdcdf5e13e6dae
Anna Fischer <anna.fischer@7unicorns.de>
2016-08-15 18:23:50 +0200

    added the possiblity to list and edit users to the backend

---
+++ app/admin/user.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register User do
+permit_params :name, :organization, :email, :created_at
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :organization
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :email
+  filter :organization
+  filter :created_at
+
+  form do |f|
+    f.inputs 'User Details' do
+      f.input :name
+      f.input :email
+      f.input :organization
+    end
+    f.actions
+  end
+end
