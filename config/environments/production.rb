48c400f400cb4dac441b5bceedeae06e6906a898
Konstantin K <konstantin.koss@gmail.com>
2016-08-23 16:55:48 +0200

    update production mailer data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,24 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "d21_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'buntundverbindlich.de' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'buntundverbindlich.de' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'mail.greensta.de',
+    port: '587',
+    domain: 'buntundverbindlich.de',
+    user_name: Rails.application.secrets.smtp_user,
+    password: Rails.application.secrets.smtp_pwd,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
