af9826ee823b0e056d145d1bcbd7780e5835ba34
Konstantin K <konstantin.koss@gmail.com>
2016-08-23 17:45:28 +0200

    devise mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'buntundverbindlich.de' }
+  config.action_mailer.default_url_options = {
+    host: 'beta.buntundverbindlich.de'
+  }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'd21@example.com'
+  config.mailer_sender = 'redaktion@buntundverbindlich.de'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
