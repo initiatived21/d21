fe058ab211417750a13b0f875e2e78f49e2970fe
Marc Altmann <mail@marcaltmann.com>
2016-09-06 15:34:54 +0200

    Add server flash messages to state

--- client/app/Flash/components/Flash.jsx
+++ client/app/Flash/components/Flash.jsx
@@ -3,9 +3,26 @@ import { Provider } from 'react-redux'
 import store from '../../lib/store'
 import FlashMessageListContainer from '../containers/FlashMessageListContainer'
 import RootComponent from '../../lib/Base/components/RootComponent'
+import { addFlashMessageAction } from '../actions/flashActions'
 
 export default class Flash extends RootComponent {
   static propTypes = {
+    flash: PropTypes.object.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+
+  componentDidMount() {
+    // Add server flash messages to state
+    const { flash } = this.props
+
+    for (let type in flash) {
+      let text = flash[type]
+      store.dispatch(addFlashMessageAction(type, text))
+    }
   }
 
   render() {
