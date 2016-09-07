93ff604fc48299e11a6b3547c04df094b48a5149
Marc Altmann <mail@marcaltmann.com>
2016-09-07 12:15:58 +0200

    Add Redis configuration for Sidekiq for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,13 @@ schedule_file = 'config/schedule.yml'
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://10.0.3.113:6379/4' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://10.0.3.113:6379/4' }
+  end
+end
