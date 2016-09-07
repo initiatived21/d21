02b397cac16f73a4b25d20537d856e5c1fe3030f
Marc Altmann <mail@marcaltmann.com>
2016-09-07 12:36:30 +0200

    Try something else with Redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,12 +4,12 @@ if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://10.0.3.113:6379/4' }
-  end
+redis = { url: 'redis://10.0.3.113:6379/4' }
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://10.0.3.113:6379/4' }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = redis
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis
 end
