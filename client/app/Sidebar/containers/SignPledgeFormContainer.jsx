6827a1f611047af3c38b580c069b4ffbf2b41d94
Marc Altmann <mail@marcaltmann.com>
2016-09-07 16:27:24 +0200

    Display flash message for sign pledge form too

--- client/app/Sidebar/containers/SignPledgeFormContainer.jsx
+++ client/app/Sidebar/containers/SignPledgeFormContainer.jsx
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux'
+import I18n from 'i18n-js'
 import SignPledgeForm from '../components/SignPledgeForm'
+import { addFlashMessageAction } from '../../Flash/actions/flashActions'
 import signPledgeAction from '../actions/signPledgeAction'
 
 const mapStateToProps = (state, ownProps) => ({
@@ -22,6 +24,7 @@ const mergeProps = function(stateProps, dispatchProps, ownProps) {
 
     onResponse: function(response) {
       if (response.status === 'success') {
+        dispatch(addFlashMessageAction('success', I18n.t('SignPledgeForm.signed_message')))
         dispatch(signPledgeAction(id))
       }
     }
