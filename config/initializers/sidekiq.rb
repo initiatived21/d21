cb34593d66c7df9216409bdc0a819206e1abe297
Marc Altmann <mail@marcaltmann.com>
2016-09-07 15:02:11 +0200

    Remove Redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,13 +3,3 @@ schedule_file = 'config/schedule.yml'
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
-
-redis = { url: 'redis://10.0.3.113:6379/4' }
-
-Sidekiq.configure_server do |config|
-  config.redis = redis
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = redis
-end
