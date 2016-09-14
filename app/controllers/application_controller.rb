549ad6847e093e781c0899c3e01e90564f3b4a78
Marc Altmann <mail@marcaltmann.com>
2016-09-14 16:23:24 +0200

    Go to admin page after admin login for #146

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    profile_path
+    if resource.is_a? AdminUser
+      admin_root_path
+    else
+      profile_path
+    end
   end
 
   protected
