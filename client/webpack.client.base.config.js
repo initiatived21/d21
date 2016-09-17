1cfe01b58dddca0881a45a59e142d3a6ae4f41fb
Marc Altmann <mail@marcaltmann.com>
2016-09-17 12:35:27 +0200

    Possible fix for #239

--- client/webpack.client.base.config.js
+++ client/webpack.client.base.config.js
@@ -60,6 +60,7 @@ module.exports = {
     }),
 
     new webpack.ProvidePlugin({
+      'Promise': 'es6-promise',
       'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     })
   ],
