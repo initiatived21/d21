355a33aaf7e1426aee24de30eaac09cb81a6a17d
Marc Altmann <mail@marcaltmann.com>
2016-09-26 10:59:54 +0200

    Display newlines in pledge descriptions for #277

--- client/app/PledgeView/components/PledgeDescription.jsx
+++ client/app/PledgeView/components/PledgeDescription.jsx
@@ -5,10 +5,31 @@ export default class PledgeDescription extends ChildComponent {
   static propTypes = {
     children: PropTypes.string.isRequired,
     className: PropTypes.string
-  };
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.childrenWithLineBreaks = this.childrenWithLineBreaks.bind(this)
+  }
+
+  childrenWithLineBreaks() {
+    const rows = this.props.children.split(/\r\n|\n/g),
+      num_rows = rows.length,
+      rowsWithLineBreaks = []
+
+    rows.forEach(function(row, index) {
+      rowsWithLineBreaks.push(row)
+      if (index !== num_rows - 1) {
+        rowsWithLineBreaks.push(<br/>)
+      }
+    })
+
+    return rowsWithLineBreaks
+  }
 
   render() {
-    const { children, className } = this.props
+    const { className } = this.props
 
     let combinedClassName = 'c-pledge__description'
     if (className) {
@@ -17,7 +38,7 @@ export default class PledgeDescription extends ChildComponent {
 
     return (
       <p className={combinedClassName}>
-        {children}
+        {this.childrenWithLineBreaks()}
       </p>
     )
   }
--- client/app/PledgeView/components/PledgeDescription_test.js
+++ client/app/PledgeView/components/PledgeDescription_test.js
@@ -14,6 +14,30 @@ describe('<PledgeDescription />', function () {
     wrapper.text().should.equal('Dies ist die Beschreibung des Versprechens.')
   })
 
+  it('should render carriage return + newline combinations as <br/> tags', function () {
+    const wrapper = shallow(
+      <PledgeDescription>
+        {'Dies ist die Beschreibung\r\ndes Versprechens.\r\n'}
+      </PledgeDescription>
+    )
+
+    wrapper.html().should.equal(
+      '<p class="c-pledge__description">Dies ist die Beschreibung<br/>des Versprechens.<br/></p>'
+    )
+  })
+
+  it('should render newlines as <br/> tags', function () {
+    const wrapper = shallow(
+      <PledgeDescription>
+        {'Dies ist die Beschreibung\ndes Versprechens.\n'}
+      </PledgeDescription>
+    )
+
+    wrapper.html().should.equal(
+      '<p class="c-pledge__description">Dies ist die Beschreibung<br/>des Versprechens.<br/></p>'
+    )
+  })
+
   it('should "pass through" its className prop', function() {
     const wrapper = shallow(<PledgeDescription className="dummy">test</PledgeDescription>)
 
