f591c67cadb3362e8b946815354e9a8ef4a5eb93
Konstantin K <konstantin.koss@gmail.com>
2016-09-30 12:24:01 +0200

    for #258 - add table of contents to FAQ page

--- app/cells/faq_cell.rb
+++ app/cells/faq_cell.rb
@@ -8,16 +8,20 @@ class FaqCell < Cell::ViewModel
   private
 
   def question_answer_sets
+    return @set if @set
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     translations = I18n.t('pages.faq')
     i = 1
-    set = {}
+    @set = {}
 
     while (translation = translations[:"question#{i}"])
-      set[markdown.render(translation)] = markdown.render(translations[:"answer#{i}"])
+      @set[markdown.render(translation)] = {
+        index: i,
+        answer: markdown.render(translations[:"answer#{i}"])
+      }
       i += 1
     end
 
-    set
+    @set
   end
 end
