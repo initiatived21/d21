820c7c473dc695fe125180c5581fc16d23403f3e
Anna Fischer <annakress@users.noreply.github.com>
2016-10-28 12:50:32 +0200

    added pledge title to acrtiveadmin index (#358)

--- app/admin/signature.rb
+++ app/admin/signature.rb
@@ -1,4 +1,26 @@
 ActiveAdmin.register Signature do
   permit_params :name, :email, :organization, :reason, :contact_person,
                 :anonymous, :confirmed
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :organization
+    column :contact_person
+    column :pledge, sortable: 'pledges.title'
+    column :reason
+    column :anonymous
+    column :created_at
+    column :confirmed
+    actions
+  end
+
+controller do
+  def scoped_collection
+    super.includes :pledge # prevents N+1 queries to your database
+  end
+end
+
 end
