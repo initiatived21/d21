776ebea55e7dfc963d289daea1880ea7f62fcf0b
Marc Altmann <marcaltmann@posteo.de>
2017-01-03 13:22:34 +0100

    Add cache-busting query string to card image url

--- app/helpers/pledges_helper.rb
+++ app/helpers/pledges_helper.rb
@@ -2,4 +2,11 @@ module PledgesHelper
   def card_available?(pledge, locale)
     pledge.send("card_#{locale}").present?
   end
+
+  def card_image_url(pledge, locale)
+    # Time-based query string to bust Facebook's/Twitter's cache.
+    # Maybe use a digest based on the actual image data in the future.
+    query_part = Time.now.to_i
+    "#{request.base_url}#{pledge.send('card_' + locale.to_s)}?v=#{query_part}"
+  end
 end
--- test/helpers/pledges_helper_test.rb
+++ test/helpers/pledges_helper_test.rb
@@ -3,6 +3,12 @@ require_relative '../test_helper'
 describe PledgesHelper do
   let(:pledge) { mock() }
 
+  def request
+    object = mock()
+    object.stubs(:base_url).returns('dummy_base_url')
+    object
+  end
+
   describe '#card_available?' do
     it 'should return false if pledge has no card for de locale' do
       pledge.stubs(:card_de).returns(nil)
@@ -22,6 +28,14 @@ describe PledgesHelper do
     it 'should return true if pledge has card for en locale' do
       pledge.stubs(:card_en).returns('/dummy/path')
       assert card_available?(pledge, :en)
+    end
+  end
+
+  describe '#card_image_url' do
+    it 'should return url with cache-busting query string for de locale' do
+      pledge.stubs(:card_de).returns('/dummy/path')
+      assert_match(/^dummy_base_url\/dummy\/path\?v=\d+$/,
+        card_image_url(pledge, :de))
     end
   end
 end
